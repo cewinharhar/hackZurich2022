a92f28df2dc9553d598497d0c427f434e6493801
afrodisiac <eric.gericke2@gmail.com>
2022-09-17 21:36:10 +0200

    added scoring method

--- company.py
+++ company.py
@@ -18,6 +18,7 @@ class Company:
         self.years =self._get_year()
         self.data = self._create_data()
 
+
     def _create_data(self):
         conn = get_db_connection()
         curs = conn.cursor()
@@ -49,15 +50,51 @@ class Company:
         print(self.data)
         return self.name
 
-    # def monthly_difference(self, year, month):
-    #     self.data
+    def _create_summed_scores(self):
+        score_compare = 0
+        my_score = self._my_score("2020", 9)
+        industries = self._get_industries() # id and industry
+        for industry in industries:
+            data = self._pull_all_values(2022, 9, industry['id'])
+            for d in data:
+                if self.industry == 'Pharmaceutical' and industry['industry'] == 'Pharmaceutical':
+                    score_compare += (0.75 * float(d["electricity"]) + 0.125 * float(d["water"]) + 0.125* float(d["co2"]))
+                if self.industry == 'Software & Tech Services' and industry['industry'] == 'Software & Tech Services':
+                    score_compare += (0.75 * float(d["electricity"]) + 0.125 * float(d["water"]) + 0.125* float(d["co2"]))
+                if self.industry == 'Electrical Equipment & Parts' and industry['industry'] == 'Electrical Equipment & Parts':
+                    score_compare += (0.75 * float(d["electricity"]) + 0.125 * float(d["water"]) + 0.125* float(d["co2"]))
+                if self.industry == 'Retail' and industry['industry'] == 'Retail':
+                    score_compare += (0.75 * float(d["electricity"]) + 0.125 * float(d["water"]) + 0.125* float(d["co2"]))
+                if self.industry == 'Insurance' and industry['industry'] == 'Insurance':
+                    score_compare += (0.75 * float(d["electricity"]) + 0.125 * float(d["water"]) + 0.125* float(d["co2"]))
+        score_compare = score_compare/2
+        final_score = (my_score/score_compare )*100
+        return final_score
 
-######
-# to show the order of results
-######
+    def _pull_all_values(self, year, month, id):
+        conn = get_db_connection()
+        curs = conn.cursor()
+        results = curs.execute("SELECT electricity, water, co2 FROM consumptions WHERE year = ? and month = ? and company_id = ?", (year, month, id))
+        data = results.fetchall()
+        conn.close()
+        return data
 
-# c = Company("sbb", 1, "bar", "fooo")
+    def _get_industries(self):
+        conn = get_db_connection()
+        curs = conn.cursor()
+        results = curs.execute("SELECT DISTINCT id, industry FROM company")
+        data = results.fetchall()
+        conn.close()
+        return data
 
-# prev_month, prev_elec, curr_month, curr_elec = get_values(c.data, "2010", 5)
+    def _my_score(self, year, month):
 
-# print(prev_month, prev_elec, curr_month, curr_elec)
+        prev_month, prev_elec, curr_month, elec = get_values(self.data, year, month)
+        prev_month, prev_value, curr_month, water = get_values(self.data, year, month, type = "water")
+        prev_month, prev_value, curr_month, co2 = get_values(self.data, year, month, type = "co2")
+        return (0.75 * float(elec) + 0.125 * float(water) + 0.125* float(co2))
+    
+c = Company("sbb", 0, "Retail", "foo")
+
+final = c._create_summed_scores()
+print(final)
