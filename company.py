0bed0fd05afa5d0091d9e85f15e18efdc6c8fec0
afrodisiac <eric.gericke2@gmail.com>
2022-09-17 02:24:17 +0200

    removed previous year field

--- company.py
+++ company.py
@@ -1,21 +1,24 @@
+# class with all the fields of the company
+# add a short summary of the company
 class Company:
     def __init__(self, name, electricity_usage_current, electricity_usage_previous, industry, year):
         self.name = name
         self.elec_current = electricity_usage_current
-        self.elec_prev = electricity_usage_previous
+        
         self.electricy_usage_per_year = {}
         self.industry = industry
         self.year = year
 
+    # method for calculating the % energy reduced / increased
+    def elec_difference(self, previous_elec):
+        return (((self.elec_current - previous_elec)/previous_elec)*100)
 
-    def elec_difference(self):
-        return (((self.elec_current - self.elec_prev)/self.elec_prev)*100)
-
+    # method for adding the current electricity to the current year
     def add_electricity_usage(self):
         self.electricy_usage_per_year[self.year] = self.elec_current
 
 
 
-comp = Company("sbb", 1000, 900, "manufactoring", 2022)
+comp = Company("sbb", 1000, "manufactoring", 2022)
 comp.add_electricity_usage()
-print(comp.elec_difference(), comp.electricy_usage_per_year)
+print(comp.elec_difference(900), comp.electricy_usage_per_year)
